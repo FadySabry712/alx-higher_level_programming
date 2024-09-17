4821b6a15074b4852929f81475614e5237cf98f4
FadySabry712 <fadysabry95@yahoo.com>
2024-09-16 20:42:13 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     retur = ""
-    for x in range(len(my_string)):
-        if (my_string[x] != 'c' and my_string[x] != 'C'):
-            retur += my_string[x]
+    for i in range(len(my_string)):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            retur += my_string[i]
         return retur
