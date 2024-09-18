f610816f4f32d8210f9ba7914342bd22c45eb019
FadySabry712 <fadysabry95@yahoo.com>
2024-09-17 20:56:44 -0700

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    sentencelen = len(sentence)
-    if sentencelen > 0:
-        return (sentencelen, sentence[0])
+    s_len = len(sentence)
+    if s_len > 0:
+        return (s_len, sentence[0])
     return (0, None)i
