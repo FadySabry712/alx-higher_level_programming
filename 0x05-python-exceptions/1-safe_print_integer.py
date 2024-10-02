4aceaffbc49c9d12a7d5ae00324ec7d99d7b2b58
FadySabry712 <fadysabry95@yahoo.com>
2024-10-02 11:22:59 -0700

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    value = isdigit()
     try:
-        if value == True:
-            print("{:d}".format(value))
-            return True
+        print("{:d}".format(value))
+        return True
     except (TypeError, ValueError):
-            return False
-
+        return False
