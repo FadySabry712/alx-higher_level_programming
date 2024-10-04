78f3267939288609f4cfa0958f9388700def4888
FadySabry712 <fadysabry95@yahoo.com>
2024-10-04 15:30:08 -0700

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+
+    sum_num = 0 
+
+    added_int = []
+
+    for i in my_list:
+        if i is not in added_int:
+            added_int.append(i)
+            sum_num += i
+    return sum_num
