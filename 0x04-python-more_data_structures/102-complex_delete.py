56a10a0449408fa989fde96be492a5d6d68ead21
FadySabry712 <fadysabry95@yahoo.com>
2024-10-05 13:12:31 -0700

    task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for k, v in a_dictionary:
+    for k, v in list(_dictionary.items()):
         if v == value:
             a_dictionary.pop(k)
     return a_dictionary
