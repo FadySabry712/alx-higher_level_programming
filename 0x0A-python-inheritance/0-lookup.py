8c4264fcc5945fad2071e4ca938691579da1e42c
FadySabry712 <fadysabry95@yahoo.com>
2024-10-14 20:13:33 -0700

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module for 0-lookup.py
+'''
+
+
+def lookup(obj):
+    """ returns the list of available attributes and methods """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+ this is to loearn about inheritance in python
