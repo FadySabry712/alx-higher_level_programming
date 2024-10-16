e3363e27fcbac13ff1c51420286e8be55448d4e8
FadySabry712 <fadysabry95@yahoo.com>
2024-10-16 15:39:10 -0700

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Make read_file function """
+""" Make append_file function """
 
 
-def append_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ Reads file name with UTF-8 encoding """
     with open(filename, "a", encoding='UTF-8') as f:
         return f.write(text)
