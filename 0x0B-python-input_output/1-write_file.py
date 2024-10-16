e080fe1863fdd5fd70d8716c3a5308903de06a5d
FadySabry712 <fadysabry95@yahoo.com>
2024-10-16 15:36:06 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """ Make read_file function """
 
 
-def read_file(filename="", text=""):
+def write_file(filename="", text=""):
     """ Reads file name with UTF-8 encoding """
     with open(filename, "w", encoding='UTF-8') as f:
         return f.write(text)
