41bb7fe2db774d3f0365da43426f5641c86952f8
FadySabry712 <fadysabry95@yahoo.com>
2024-12-16 19:57:31 -0800

    task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/* add */
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
+}
+
+add(a, b);
