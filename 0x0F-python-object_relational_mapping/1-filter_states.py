6e0df604d0324b8b112569a69e31561ed7df0e65
FadySabry712 <fadysabry95@yahoo.com>
2024-12-22 22:48:40 -0800

    task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
+
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=db_name)
+    myC = db.cursor()
+    myC.execute("SELECT states.id, name FROM states WHERE name "
+                "COLLATE latin1_general_cs "
+                "LIKE 'N%' "
+                "ORDER BY states.id ASC;")
+    rows = myC.fetchall()
+    for row in rows:
+        print(row)
+
+    myC.close()
+    db.close()
