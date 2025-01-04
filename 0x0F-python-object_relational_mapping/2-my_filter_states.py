df8f6c1f80ba0e8d03683d396aae3e90f21c9fcc
FadySabry712 <fadysabry95@yahoo.com>
2025-01-04 02:56:35 -0800

    modifed task 2, 9, 10

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,28 +1,18 @@
 #!/usr/bin/python3
-""" Filter states by given user input """
+"""takes in an argument and displays all values
+in the states table """
 
-from sys import argv
-import MySQLdb
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    state_name = argv[4]
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=db_name)
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
     myC = db.cursor()
-
-    query = """
-    SELECT states.id, name FROM states WHERE name='{:s}'
-    COLLATE latin1_general_cs
-    ORDER BY states.id ASC;
-    """.format(state_name)
-
-    myC.execute(query)
+    myC.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY states.id ASC".format(sys.argv[4]))
     rows = myC.fetchall()
     for row in rows:
         print(row)
